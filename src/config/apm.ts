0b6212002211881000251344f21c22f560a48875
ardi95 <ardiansyahpratama95@gmail.com>
2025-12-07 12:30:12 +0700

    add on off apm

--- src/config/apm.ts
+++ src/config/apm.ts
@@ -1,19 +1,20 @@
-// Add this to the very top of the first file loaded in your app
-// Jangan jalankan APM saat testing
-const apm = process.env.NODE_ENV === 'testing' 
-  ? null 
-  : require('elastic-apm-node').start({
+const shouldStartApm =
+  process.env.NODE_ENV !== 'testing' && process.env.APP_APM_ACTIVE === 'true';
+
+const apm = shouldStartApm
+  ? require('elastic-apm-node').start({
       serviceName: 'be-app-management',
       apiKey: process.env.APM_API_KEY || '',
       serverUrl: process.env.APM_SERVER_URL || 'http://localhost:8200',
       environment: process.env.NODE_ENV || 'development',
       logLevel: 'info',
-      captureBody: 'all', // Capture request/response body
-      captureHeaders: true, // Capture headers
+      captureBody: 'all',
+      captureHeaders: true,
       captureExceptions: true,
       captureSpanStackTraces: true,
-      transactionSampleRate: 1.0, // 100% sampling
-      errorSampleRate: 1.0, // 100% error sampling
-    });
+      transactionSampleRate: 1.0,
+      errorSampleRate: 1.0,
+    })
+  : null;
 
 export { apm };
